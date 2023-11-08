0fbb01a98a8045fb160719d656f4f3565af3fd39
Capsaicin1 <109123242+Capsaicin1@users.noreply.github.com>
2023-11-09 11:06:26 +1300

    v.0.3 Grid size buttons generate correct number of divs. Color buttons don't work. Grid doesn't get duplicated.

--- src/main.js
+++ src/main.js
@@ -3,28 +3,44 @@
 // const div = document.querySelector(".canvas-container div");
 // div.innerHTML = "<p>";
 
-const generateDiv = () => {
+const removeCurrentStyle = (buttons) => {
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+};
+
+const generateDiv = (divNum = 20 * 20, className = "grid-20x20") => {
   const canvasContainer = document.querySelector(".canvas-container");
-  for (let i = 0; i < 10; i++) {
-    let div = document.createElement("div");
-    div.style.color = "red";
-    div.style.width = "48px";
-    div.style.height = "48px";
-    div.classList.add("div");
-    canvasContainer.appendChild(div);
+  canvasContainer.innerHTML = "";
+  for (let i = 0; i < divNum; i++) {
+    let gridDiv = document.createElement("div");
+    canvasContainer.classList.remove("grid-10x10", "grid-20x20", "grid-30x30");
+    canvasContainer.classList.add(className);
+    gridDiv.style.width = "10px";
+    gridDiv.style.height = "10px";
+    canvasContainer.appendChild(gridDiv);
   }
 };
 
-const displayGrid = () => {
+const chooseGrid = () => {
   const sizeButtons = document.querySelectorAll(".circle");
+  const colorButtons = document.querySelectorAll(".rectangle");
   sizeButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      if (button.className.includes("active")) return;
-
-      button.setAttribute("class", `${button.className} active`);
-      generateDiv();
+      removeCurrentStyle(sizeButtons);
+      removeCurrentStyle(colorButtons);
+      if (button.classList.contains("10x10")) {
+        sizeButtons[0].classList.add("active");
+        generateDiv(10 * 10, "grid-10x10");
+      } else if (button.classList.contains("20x20")) {
+        sizeButtons[1].classList.add("active");
+        generateDiv(20 * 20, "grid-20x20");
+      } else if (button.classList.contains("30x30")) {
+        sizeButtons[2].classList.add("active");
+        generateDiv(30 * 30), "grid-30x30";
+      }
     });
   });
 };
 
-displayGrid();
+chooseGrid();
